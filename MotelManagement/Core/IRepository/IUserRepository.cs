d843b76605c44634c086c5abf97f197df14596bc
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-03-03 21:58:11 +0700

    Register and upload file

---
+++ MotelManagement/Core/IRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using MotelManagement.Data.Models;
+
+namespace MotelManagement.Core.IRepository
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        void AddUserRegister(User user);
+    }
+}
