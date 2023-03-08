6b98ed639920f4aa16dc49226bd06f63a362f14a
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-03-08 13:02:19 +0700

    Booking

---
+++ MotelManagement/Business/IService/IContractService.cs
@@ -0,0 +1,9 @@
+﻿using MotelManagement.Data.Models;
+
+namespace MotelManagement.Business.IService
+{
+    public interface IContractService
+    {
+        public Task<List<Contract>> getListContractsByUserId(int userId);
+    }
+}
