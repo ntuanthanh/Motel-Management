ddca0147aaf375f51deda77ed8beb90a57fffc42
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-03-15 00:34:06 +0700

    fe list booking

---
+++ MotelManagement/Pages/admin/ListBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotelManagement.Pages.admin
+{
+    public class ListBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
