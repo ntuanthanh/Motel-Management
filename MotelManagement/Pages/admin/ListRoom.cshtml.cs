26517e3c549b382beca0ec3e16f38a3049eca492
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-03-25 16:59:27 +0700

    fd list admin

---
+++ MotelManagement/Pages/admin/ListRoom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotelManagement.Pages.admin
+{
+    public class ListRoomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
