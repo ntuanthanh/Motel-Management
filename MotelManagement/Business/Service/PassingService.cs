be7620b70ba44ff93e357c51c7bba7d6ab56c6c0
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-03-27 00:30:23 +0700

    passing

--- MotelManagement/Business/Service/PassingService.cs
+++ MotelManagement/Business/Service/PassingService.cs
@@ -16,6 +16,22 @@ namespace MotelManagement.Business.Service
         {
             return await _unitOfWork.passingRepository.isBookingPassing(roomid, userId);
         }
+
+        public async Task<bool> isRoomRented(int? roomId)
+        {
+            return await _unitOfWork.roomRepository.isRoomRentedReal(roomId);
+        }
+        public async Task<bool> isRoomWaiting(int? roomId)
+        {
+            return await _unitOfWork.roomRepository.isRoomWaiting(roomId);
+        }
+
+        public async Task<List<Passing>> PassingBookingsAvailableSearching(int? roomId, string? nameBooking, string? emailBooking, string? phoneBooking, DateTime? fromBooking, DateTime? toBooking)
+        {
+            List<Passing> bookings = await _unitOfWork.passingRepository.PassingBookingListByRoomAvailableSearching(roomId, nameBooking, emailBooking, phoneBooking, fromBooking, toBooking);
+            return bookings;
+        }
+
         public async Task RegisterPassing(int userId, int roomid)
         {
             // roomId
@@ -30,6 +46,26 @@ namespace MotelManagement.Business.Service
             passing.MeetingDate = null;
             passing.MemberId = null; 
             await _unitOfWork.passingRepository.AddAsync(passing); 
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task UpdateMeetingDateAllUser(DateTime? meetingDate, int? roomId)
+        {
+            await _unitOfWork.passingRepository.updateMeetingDate(meetingDate, roomId);
+            await _unitOfWork.SaveAsync();
+        }
+        public async Task UpdateMeetingForUser(DateTime? dateTime, int? bookingId, int? roomId)
+        {
+            await _unitOfWork.passingRepository.updateMeetingDateForUser(dateTime, bookingId);
+            await _unitOfWork.SaveAsync();
+        }
+        public async Task PushUserToAdmin(int userId, int roomid, int bookingId)
+        {
+            // Update status Booking 
+            await _unitOfWork.passingRepository.updateSuccessRejectUsersExceptMember(userId, roomid, bookingId);
+            //// Update status Room
+            await _unitOfWork.roomRepository.UpdateStatusRoom(roomid, (int)ROOM_STATE.Waiting);
+            // Save transaction 
             await _unitOfWork.SaveAsync();
         }
     }
