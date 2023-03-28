e4898f8e0914b25e6f59382489d506364122d9d2
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-03-28 08:33:21 +0700

    error page

---
+++ MotelManagement/Pages/ErrorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotelManagement.Pages
+{
+    public class ErrorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
